bc07ba13770fa93321dc9accb473a1986f210c54
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-01-13 00:25:33 +0000

    [add]バリデーション

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,21 @@ class ListsController < ApplicationController
   end
 
 def create
-  list = List.new(list_params)
-  list.save
+ @list = List.new(list_params)
+ if @list.save
+  redirect_to list_path(@list.id)
+ else
+   render :new
+  end
+end
+
+def edit
+  @list=List.find(params[:id])
+end
+
+def update
+  list=List.find(params[:id])
+  list.update(list_params)
   redirect_to list_path(list.id)
 end
 
@@ -17,8 +30,14 @@ def show
   @list=List.find(params[:id])
 end
 
+def destroy
+  list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+end
+
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
